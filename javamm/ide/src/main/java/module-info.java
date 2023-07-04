aba13985512027700856865278b98b6df43eec10
odessabear <bearodessa@gmail.com>
2023-07-05 02:21:54 +0300

    Maven modules config

---
+++ javamm/ide/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * @author devonline
+ * @link https://devonline.academy/javamm
+ */
+module javamm.ide {
+    requires javamm.code;
+    requires javamm.compiler;
+    requires javamm.interpreter;
+    requires javamm.vm;
+}
